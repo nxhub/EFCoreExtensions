34a0544f03b64415aebc200b42015cde177d7117
codeping <deping.du@gmail.com>
2020-06-16 22:40:09 +0800

    项目初始化。

---
+++ src/EFCoreExtensions/EFCoreHostApplicationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class EFCoreHostApplicationExtensions
+    {
+        public static IHostApplicationLifetime TryMigrateDb<TDbContext>(
+            this IHostApplicationLifetime host,
+            IApplicationBuilder app)
+            where TDbContext : DbContext
+        {
+            host.ApplicationStarted.Register(() =>
+            {
+                try
+                {
+                    IServiceScope scope = app.ApplicationServices.CreateScope();
+
+                    TDbContext context = scope.ServiceProvider.GetService<TDbContext>();
+
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            });
+
+            return host;
+        }
+    }
+}
